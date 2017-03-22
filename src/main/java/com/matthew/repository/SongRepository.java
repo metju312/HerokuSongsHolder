cd0e37e073a2101eef12d1e1e22acf14f46e6216
Matthew <metju312@gmail.com>
2017-03-22 17:14:47 +0100

    added findByTitle method

--- src/main/java/com/matthew/repository/SongRepository.java
+++ src/main/java/com/matthew/repository/SongRepository.java
@@ -3,9 +3,13 @@ package com.matthew.repository;
 import com.matthew.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface SongRepository extends PagingAndSortingRepository<Song, Long> {
+public interface SongRepository extends PagingAndSortingRepository<Song, Long>{
+    List<Song> findByTitle(@Param("title") String title);
 }
